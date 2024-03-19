0229676ca2b71b4d1deeb08d456d8bcff23342f4
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-03-19 14:45:53 -0300

    Add custom fonts and colors to tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        'jakartaSans': ['"Plus Jakarta Sans"', 'sans-serif'],
+				'openSans': ['"Open Sans"', 'sans-serif'],
+      },
+			colors: {
+				'customPurple': '#ab96f8',
+				'customPink': '#ff99d0',
+				
+			},
+		},
 	},
 	plugins: [],
 }
